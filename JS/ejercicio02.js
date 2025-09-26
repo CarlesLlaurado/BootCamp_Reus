d924598c7c874a69d29aed6ff6e31abbd22d96ac
CarlesLlaurado <carlesllauradollaurado@gmail.com>
2025-09-26 10:20:31 +0200

    exercici02.js

---
+++ JS/ejercicio02.js
@@ -0,0 +1,28 @@
+/*
+const nombre = "Carles";
+const apellidos = "Llauradó Llauradó";
+
+const saludar = (nombre, apellidos) => {
+    return `Hola, ${nombre} ${apellidos}!`;
+}
+
+console.log(saludar(nombre, apellidos));
+*/
+
+
+/*
+
+const mostrarBoolean = (valor) => {
+    console.log(valor)
+}
+
+mostrarBoolean(true);
+mostrarBoolean(false);
+
+*/
+
+const infinitos = (...nums) => {
+    nums.forEach(num => console.log(num));
+}
+
+infinitos(1,2,3,4,5);
