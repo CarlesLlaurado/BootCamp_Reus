3adbed0deffb116176c05976320114bc5bb39361
CarlesLlaurado <carlesllauradollaurado@gmail.com>
2025-09-30 13:20:36 +0200

    display numbers

--- calculator-app-main/main.js
+++ calculator-app-main/main.js
@@ -10,3 +10,42 @@ toggle.addEventListener("click", () => {
     document.body.setAttribute("data-theme", estado);
 });
 
+const displayText = document.querySelector(".displaytext");
+
+let display = "";
+
+const teclas = document.querySelectorAll(".tecla");
+
+teclas.forEach(tecla => {
+    tecla.addEventListener("click", () => {
+        const valor = tecla.textContent;
+        
+        if(valor === "RESET") {
+            display = "";
+            displayText.textContent = "0";
+        }
+        else if(valor === "DEL") {
+            display = display.slice(0, -1);
+            displayText.textContent = display || "0";
+        }
+        else if (valor === "+") {
+            display += "+";
+            displayText.textContent = display;
+        }
+        else if (valor === "-") {
+            display += "-";
+            displayText.textContent = display;
+        }
+
+        else {
+            if (display.length >= 17) return;
+
+            display += valor;
+            displayText.textContent = display;
+        }
+
+    });
+});
+
+
+
