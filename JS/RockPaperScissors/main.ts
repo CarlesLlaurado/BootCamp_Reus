011beae29d7622fb2609d16580101cd98a3ec60a
CarlesLlaurado <carlesllauradollaurado@gmail.com>
2025-10-08 13:40:51 +0200

    rock paper scissors done

--- JS/RockPaperScissors/main.ts
+++ JS/RockPaperScissors/main.ts
@@ -12,23 +12,39 @@ const buttons = document.querySelectorAll<HTMLButtonElement>('button');
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
+        resultText.textContent = "";
         player1 = options[Math.floor(Math.random() * options.length)];
-        player1Img.src = `images/${player1}.png`;
         player2 = button.id as Choice;
-        player2Img.src = `images/${player2}.png`;
 
-        if (player1 === player2) {
-            resultText.textContent = "IT'S A TIE 👔";
-        }
-        else if (
-            (player1 === "rock" && player2 === "scissors") ||
-            (player1 === "paper" && player2 === "rock") ||
-            (player1 === "scissors" && player2 === "paper")
-        ) {
-            resultText.textContent = "🔴 RED WINS!!! 🔴";
-        }
-        else {
-            resultText.textContent = "🔵 BLUE WINS!!! 🔵";
-        }
+        player1Img.src = "images/rock.png"
+        player1Img.classList.add("shake");
+        
+        player2Img.src = "images/rock.png"
+        player2Img.classList.add("shake2");
+
+        setTimeout(() => {
+            player1Img.classList.remove("shake");
+            player2Img.classList.remove("shake2");
+            player1Img.src = `images/${player1}.png`;
+            player2Img.src = `images/${player2}.png`;
+
+            if (player1 === player2) {
+                resultText.textContent = "IT'S A TIE 👔";
+            }
+            else if (
+                (player1 === "rock" && player2 === "scissors") ||
+                (player1 === "paper" && player2 === "rock") ||
+                (player1 === "scissors" && player2 === "paper")
+            ) {
+                resultText.textContent = "🔴 RED WINS!!! 🔴";
+            }
+            else {
+                resultText.textContent = "🔵 BLUE WINS!!! 🔵";
+            }
+
+        }, 1500);
+
+
+
     })
 });
